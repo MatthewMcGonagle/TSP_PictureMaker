089c57619c45ef3487e4e51f14ec65215fbe717c
Matthew McGonagle <mwmcgonagle@gmail.com>
2019-03-10 17:16:59 -0400

    Formatting base.py to comply with pylint standards.

--- tspDraw/base.py
+++ tspDraw/base.py
@@ -1,64 +1,108 @@
+'''
+Virtual base annealing class for basic simulated annealing behavior.
+'''
+
 import numpy as np
 
 class Annealer:
+    '''
+    Virtual base class for doing simulated annealing. Handles temperature cooling,
+    running an accepted move trial, finding energy differences, doing a warm restart that
+    involves resetting counters.
 
-    def __init__(self, nSteps, vertices, temperature, tempCool):
+    Sub-classes need to redefine the following methods:
+        _make_random_pair()
+        _make_move()
+
+    Members
+    -------
+    n_steps : Int
+        Total number of steps to use for one run (when iteration stops).
+
+    vertices : Numpy array of shape (n_vertices, 2)
+        The xy-coordinates of the vertices.
+
+    temperature : Float
+        The current temperature of the annealer. Used for computing probability of
+        moving to a higher energy state.
+
+    temp_cool : Float
+        Multiplicatively changes the temperature (usually you want to reduce temperature) at
+        every iteration.
+
+    steps_processed : Int
+        The number of steps processed in the run.
+
+    n_vertices : Int
+        The number of vertices in the cycle.
+    '''
+    def __init__(self, n_steps, vertices, temperature, temp_cool):
 
         # Members from Parameters
 
-        self.nSteps = nSteps
+        self.n_steps = n_steps
         self.vertices = vertices
         self.temperature = temperature
-        self.tempCool = tempCool
+        self.temp_cool = temp_cool
 
-        self.stepsProcessed = 0
-        self.nVertices = len(vertices)
-        
+        self.steps_processed = 0
+        self.n_vertices = len(vertices)
 
     def __iter__(self):
         return self
 
     def __next__(self):
 
-        if self.stepsProcessed >= self.nSteps:
+        if self.steps_processed >= self.n_steps:
             raise StopIteration
 
-        self._updateState()
-        begin, end = self._makeRandomPair()
-        energyDiff = self._findEnergyDifference(begin, end)
-        if energyDiff < 0:
-            proposalAccepted = True
+        self._update_state()
+        begin, end = self._make_random_pair()
+        energy_diff = self._find_energy_difference(begin, end)
+        if energy_diff < 0:
+            proposal_accepted = True
         else:
-            proposalAccepted = self._runProposalTrial(energyDiff)
-        
-        if proposalAccepted:
-            self._makeMove(begin, end)
+            proposal_accepted = self._run_proposal_trial(energy_diff)
 
-        return energyDiff
+        if proposal_accepted:
+            self._make_move(begin, end)
 
-    def doWarmRestart(self):
-        self.stepsProcessed = 0
+        return energy_diff
 
-    def getCycle(self):
+    def do_warm_restart(self):
         '''
-        Get the vertices in the order they appear in the cycle. 
+        Reset the steps processed counter.
+        '''
+        self.steps_processed = 0
+
+    def get_cycle(self):
+        '''
+        Get the vertices in the order they appear in the cycle.
 
         Returns
         -------
-        Numpy array of shape (nVertices, 2)
-            The coordinates of the vertices for the order they appear in the cycle. 
+        Numpy array of shape (n_vertices, 2)
+            The coordinates of the vertices for the order they appear in the cycle.
         '''
         cycle = self.vertices.copy()
         begin = self.vertices[0]
         cycle = np.concatenate([cycle, [begin]], axis = 0)
         return cycle
 
-    def getEnergy(self):
+    def get_energy(self):
+        '''
+        Compute the energy (i.e. the length) of the current cycle.
+
+        Returns
+        -------
+        Energy : Float
+            The current energy.
+        '''
         energy = np.linalg.norm(self.vertices[1:] - self.vertices[:-1], axis = 1).sum()
         energy += np.linalg.norm(self.vertices[0] - self.vertices[-1])
         return energy
 
-    def getInfoString(self):
+    def get_info_string(self):
         '''
         Get a string for information of the current state of the iterator.
 
@@ -68,30 +112,32 @@ class Annealer:
             Contains information for the energy and the temperature.
         '''
 
-        energy = self.getEnergy()
+        energy = self.get_energy()
         info = 'Energy = ' + str(energy)
         info += '\tTemperature = ' + str(self.temperature)
 
         return info
 
-    def _updateState(self):
-        self.temperature *= self.tempCool
-        self.stepsProcessed += 1
+    def _update_state(self):
+        self.temperature *= self.temp_cool
+        self.steps_processed += 1
 
-    def _findEnergyDifference(self, i, j):
+    def _find_energy_difference(self, i, j):
         '''
         Find the energy (i.e. length) difference resulting from reversing the path between
-        the ith vertex in the cycle with the jth vertex in the cycle. Both of the indices i and j 
+        the ith vertex in the cycle with the jth vertex in the cycle. Both of the indices i and j
         should be greater than 0 (i.e. excluding the first vertex in the cycle) and less than
-        self.nVertices - 1 (i.e. excluding the last vertex in the cycle).
+        self.n_vertices - 1 (i.e. excluding the last vertex in the cycle).
 
         Parameters
         ----------
         i : Int
-            Index for the ith vertex in the cycle. Should be greater than 0 and less than self.nVertices - 1.
+            Index for the ith vertex in the cycle. Should be greater than 0 and less than
+            self.n_vertices - 1.
 
         j : Int
-            Index for the jth vertex in the cycle. Should be greater than 0 and less than self.nVertices - 1.
+            Index for the jth vertex in the cycle. Should be greater than 0 and less than
+            self.n_vertices - 1.
 
         Returns
         -------
@@ -101,32 +147,31 @@ class Annealer:
 
         begin = self.vertices[i]
         if i > 0:
-            beginParent = self.vertices[i - 1]
+            begin_parent = self.vertices[i - 1]
         else:
-            beginParent = self.vertices[self.nVertices - 1]
+            begin_parent = self.vertices[self.n_vertices - 1]
 
         end = self.vertices[j]
-        if j < self.nVertices - 1:
-            endChild = self.vertices[j + 1]
+        if j < self.n_vertices - 1:
+            end_child = self.vertices[j + 1]
         else:
-            endChild = self.vertices[0]
-       
-        oldEnergy = np.linalg.norm(begin - beginParent) + np.linalg.norm(end - endChild)
-        newEnergy = np.linalg.norm(begin - endChild) + np.linalg.norm(end - beginParent)         
+            end_child = self.vertices[0]
 
-        energyDiff = newEnergy - oldEnergy
+        old_energy = np.linalg.norm(begin - begin_parent) + np.linalg.norm(end - end_child)
+        new_energy = np.linalg.norm(begin - end_child) + np.linalg.norm(end - begin_parent)
 
-        return energyDiff
+        return new_energy - old_energy
 
-    def _runProposalTrial(self, energyDiff):
+    def _run_proposal_trial(self, energy_diff):
         '''
         Run the bernoulli trial for determining whether to accept a proposed reversal in the case
         that the reversal will result in an increase in energy.
 
         Parameters
         ----------
-        energyDiff : Float
-            The energy difference (i.e. lengthDifference) for the proposal. This should be greater than 0.
+        energy_diff : Float
+            The energy difference (i.e. lengthDifference) for the proposal. This should be greater
+            than 0.
 
         Returns
         -------
@@ -134,14 +179,14 @@ class Annealer:
             Whether to accept the proposal based on the random bernoulli trial.
         '''
 
-        prob = np.exp(-energyDiff / self.temperature)
+        prob = np.exp(-energy_diff / self.temperature)
 
         trial = np.random.uniform()
 
-        return (trial < prob)
+        return trial < prob
 
-    def _makeRandomPair(self):
-        raise Exception("_getRandomPair() is still virtual")
+    def _make_random_pair(self):
+        raise NotImplementedError()
 
-    def _makeMove(self, begin, end):
-        raise Exception("_makeMove() is still virtual") 
+    def _make_move(self, begin, end):
+        raise NotImplementedError()
