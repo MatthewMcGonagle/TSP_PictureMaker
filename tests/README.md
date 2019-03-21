b6df1b6e5ade27750e2974de1c46e46988fb16e9
Matthew McGonagle <mwmcgonagle@gmail.com>
2019-03-21 15:51:08 -0400

    For unit tests, added readme and bash script.

---
+++ tests/README.md
@@ -0,0 +1,8 @@
+# tsp_draw Unit Tests
+
+To run all of the unit tests, simply run the Bash script `run_tests.sh`, i.e. 
+from Bash, run the following
+
+```
+sh run_tests.sh
+```
---
+++ tests/run_tests.sh
@@ -0,0 +1,7 @@
+echo "Testing tsp_draw.base"
+echo "---------------------"
+python base.py
+
+echo "Testing tsp_draw.size_scale"
+echo "---------------------------"
+python size_scale.py
